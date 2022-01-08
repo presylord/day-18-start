8297d54603ee7971a40533269ef62a7ad12d5896
Presy Lord Kidlat <kidlat123@gmail.com>
2022-01-09 05:01:53 +0800

    2nd commit

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from turtle import Turtle, Screen
+import random
 
-timmy_the_turtle = Turtle()
+turtle = Turtle()
+turtle.pensize(10)
+turtle.speed("fast")
+Screen().colormode(255)
+
+angle = [0, 90, 180, 270]
+
+for _ in range(100):
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    turtle.pencolor(r, g, b)
+    r_angle = random.choice(angle)
+    turtle.setheading(r_angle)
+    turtle.forward(50)
+
+
+
+
+
 
 
 
